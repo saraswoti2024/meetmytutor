410adc81929f691e591b5b1a3283b04a9a888923
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-07-20 15:16:56 +0545

    tutor_profile_Detail view

--- findtutor/urls.py
+++ findtutor/urls.py
@@ -3,4 +3,6 @@ from findtutor import views
 
 urlpatterns = [
     path('all-tutor/',views.all_tutor_view,name="all_tutor"),       
+    path('nearby-tutor/',views.nearby_tutor_view,name="nearby_tutor"),       
+    path('view_profile/<int:id>',views.view_tutor_profile_view,name="view_tutor_profile"),       
 ]
--- findtutor/views.py
+++ findtutor/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from profileapp.models import Profile_Tutor
 from categories.models import districts,subjects_list
 from django.db.models import Q 
+from django.contrib.auth.decorators import login_required
+
 
 def all_tutor_view(request):
     all_subjects = subjects_list.objects.all()
-    all_tutors = Profile_Tutor.objects.all() 
+    all_tutors = Profile_Tutor.objects.all().order_by('id') 
     districts_list = districts.objects.all()
 
     education_level = request.GET.get('education_level')
@@ -16,22 +18,13 @@ def all_tutor_view(request):
     if education_level:
         all_tutors = all_tutors.filter(education_data__contains=[{'level': education_level}])
 
-
     if subject:
-        # This is the correct and efficient way to filter for subjects within the JSONField.
-        # It checks if any object in 'education_data' list has a 'subjects' array
-        # that contains the 'subject' value.
         all_tutors = all_tutors.filter(education_data__contains=[{'subjects': [subject]}])
 
-    # Filter by district
     if district_name:
-        # Assumes Profile_Tutor has a direct 'district' field.
         all_tutors = all_tutors.filter(district__iexact=district_name)
 
-    # Filter by tutor_name (first name or last name)
     if tutor_name:
-        # Assuming Profile_Tutor has a ForeignKey to User model called 'user'
-        # And the User model has 'first_name' and 'last_name'
         all_tutors = all_tutors.filter(
             Q(user__first_name__icontains=tutor_name) |
             Q(user__last_name__icontains=tutor_name)
@@ -39,11 +32,23 @@ def all_tutor_view(request):
 
     context = {
         'tutorlist': all_tutors,
-        'district': districts_list, # Pass the list of all districts
-        'selected_education_level': education_level, # Pass back selected values for form persistence
+        'district': districts_list, 
+        'selected_education_level': education_level,
         'selected_subject': subject,
         'selected_district': district_name,
         'searched_tutor_name': tutor_name,
-        'all_subjects' : all_subjects, # Pass all subjects for the dropdown
+        'all_subjects' : all_subjects, 
     }
     return render(request, 'findtutor/all_tutor.html', context)
+
+@login_required(login_url='log_in')
+def nearby_tutor_view(request):
+    return render(request,'findtutor/nearby_tutor.html')
+
+@login_required(login_url='log_in')
+def view_tutor_profile_view(request,id):
+    profile_id = Profile_Tutor.objects.get(id=id)
+    context = {
+        'profile_id' : profile_id
+    }
+    return render(request,'profile_detail/tutor_view_profile.html',context)
